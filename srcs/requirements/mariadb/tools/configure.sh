66c01c4706e112d1052f0b656a2740cf858e8d3a
Jay13Jeong <38svbe@gmail.com>
2022-09-23 20:42:23 +0900

    22_09_23_20:42

--- srcs/requirements/mariadb/tools/configure.sh
+++ srcs/requirements/mariadb/tools/configure.sh
@@ -3,14 +3,15 @@
 mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 # chown -R mysql:myscd /var/lib/mysql
-mysql_install_db --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+# mysql_install_db --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+mysql_install_db --datadir=/var/lib/mysql --user=mysql
 mysqld --user=mysql --bootstrap <<EOF
 USE mysql;
 FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PWD';
-CREATE DATABASE $WP_DATABASE_NAME;
-CREATE USER '$WP_DATABASE_USR'@'%' IDENTIFIED by '$WP_DATABASE_PWD';
-GRANT ALL PRIVILEGES ON $WP_DATABASE_NAME.* TO '$WP_DATABASE_USR'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';
+CREATE DATABASE $MARIADB_DATABASE;
+CREATE USER '$MARIADB_USER'@'%' IDENTIFIED by '$MARIADB_PASSWORD';
+GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
 
