eca8c75527b634c3cdb1ba3ee3bd63f52eea811f
Jay13Jeong <63899204+Jay13Jeong@users.noreply.github.com>
2022-11-18 19:47:03 +0900

    remove : old data

--- Makefile
+++
@@ -1,7 +0,0 @@
-all:
-	docker-compose -f srcs/docker-compose.yml down
-	rm -rf ${HOME}/data
-	docker system prune -f
-	docker-compose -f srcs/docker-compose.yml up --build
-
-.PHONY: all
--- srcs/requirements/bonus/adminer/tools/configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p /var/www/html
-wget https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-mysql-en.php -O /var/www/html/adminer.php
-
-/usr/sbin/php-fpm7
